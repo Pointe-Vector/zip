8f5f11aac51343441c22b84649de9b271ed8cc5a
Andrew Hoekstra <andrew@pointevector.com>
2022-11-22 21:31:20 -0500

    Convert to new API

--- pointevector/zip/parser.py
+++ pointevector/zip/parser.py
@@ -1,140 +1,156 @@
-from abc import ABC, abstractmethod
-from enum import Enum
+import enum
 import logging
 import io
-import struct
 import zlib
 
-from pointevector.zip.core import DataDescriptor, FileHeader, CDHeader, EOCD, IncompleteParse
+from pointevector.zip import (
+    _central_directory_header,
+    _data_descriptor,
+    _eocd,
+    _local_header,
+    _zip64_eocd,
+    _zip64_eocd_locator,
+    exceptions,
+)
 
-class StreamParser(ABC):
-    class State(Enum):
+
+class StreamParser:
+    class _State(int, enum.Enum):
         UNKNOWN = 0
         LOCAL_HEADER = 1
-        CONTENT = 2
-        DATA_DESCRIPTOR = 3
-        CD_HEADER = 4
-        EOCD = 5
+        DATA_DESCRIPTOR = 2
+        CONTENT = 3
+        CENTRAL_DIRECTORY_HEADER = 4
+        ZIP64_EOCD = 5
+        ZIP64_EOCD_LOCATOR = 6
+        EOCD = 7
 
     def __init__(self):
-        self.zip_state = StreamParser.State.UNKNOWN
+        self.zip_state = StreamParser._State.LOCAL_HEADER
         self.buffer = io.BytesIO()
         self.decompressor = None
-
-    @abstractmethod
-    def local_header(self, data: FileHeader):
-        pass
-
-    @abstractmethod
-    def file_content(self, data: bytes):
-        pass
-
-    @abstractmethod
-    def data_descriptor(self, data: DataDescriptor):
-        pass
-
-    @abstractmethod
-    def cd_header(self, data: CDHeader):
-        pass
-
-    @abstractmethod
-    def eocd(self, data: EOCD):
-        pass
+        self.header = None
+        self.expect_descriptor = False
+        self.content = io.BytesIO()
+        self.end_of_files = False
 
     def feed(self, data: bytes):
+        logger = logging.getLogger()
+
         # Update buffer
-        self.buffer.seek(self.buffer.getbuffer().nbytes)
         self.buffer.write(data)
 
         # Process buffer
         while True:
-            # Unknown
-            if self.zip_state == StreamParser.State.UNKNOWN:
-                # Ensure there is enough data in the buffer
-                if self.buffer.getbuffer().nbytes < 4:
-                    break
-                self.buffer.seek(0)
-                magic = self.buffer.read(4)
-                if magic == FileHeader.MAGIC_NUMBER:
-                    self.zip_state = StreamParser.State.LOCAL_HEADER
-                elif magic == DataDescriptor.MAGIC_NUMBER:
-                    self.zip_state = StreamParser.State.DATA_DESCRIPTOR
-                elif magic == CDHeader.MAGIC_NUMBER:
-                    self.zip_state = StreamParser.State.CD_HEADER
-                elif magic == EOCD.MAGIC_NUMBER:
-                    self.zip_state = StreamParser.State.EOCD
-                else:
-                    logging.critical('bad state')
-                    raise Exception(magic)
-            # Local Header
-            elif self.zip_state == StreamParser.State.LOCAL_HEADER:
-                # Ensure there is enough data in the buffer
-                if self.buffer.getbuffer().nbytes < struct.calcsize(FileHeader.STATIC_FORMAT):
-                    break
-                self.buffer.seek(0)
+            buf = self.buffer.getbuffer()
+            logger.info("%s %s", self.zip_state, buf[:4].tobytes())
+            if self.zip_state == StreamParser._State.LOCAL_HEADER:
+                # Attempt to parse local header
                 try:
-                    data = FileHeader.from_stream(self.buffer)
-                except IncompleteParse:
+                    self.header = _local_header.LocalHeader.from_memoryview(buf)
+                except exceptions.IncompleteParse:
                     break
-                self.local_header(data)
-                self.buffer = io.BytesIO(self.buffer.read())
-                # Prepare CONTENT state
-                self.decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
-                self.content = io.BytesIO()
-                self.zip_state = StreamParser.State.CONTENT
-            # Content
-            elif self.zip_state == StreamParser.State.CONTENT:
-                self.buffer.seek(0)
-                data = self.decompressor.decompress(self.buffer.read())
-                self.file_content(data)
-                # Reset buffer
+                except exceptions.BadMagic:
+                    if buf[:4] == _central_directory_header.MAGIC:
+                        self.zip_state = StreamParser._State.CENTRAL_DIRECTORY_HEADER
+                        continue
+                    else:
+                        raise exceptions.BadMagic(buf[:4].tobytes())
+
+                # Check compatibility
+                if self.header.bit_flags.is_encrypted:
+                    raise NotImplementedError()
+                if self.header.min_version_needed > 20:
+                    raise NotImplementedError()
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(self.header) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
+                self.decompressor = zlib.decompressobj(wbits=-zlib.MAX_WBITS)
+                self.zip_state = StreamParser._State.CONTENT
+            elif self.zip_state == StreamParser._State.CONTENT:
+                self.content.write(self.decompressor.decompress(buf))
                 self.buffer = io.BytesIO(self.decompressor.unused_data)
-                # Transition to UNKNOWN state
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
                 if self.decompressor.eof:
-                    self.zip_state = StreamParser.State.UNKNOWN
+                    yield self.header, self.content
+                    if self.header.bit_flags.expect_descriptor:
+                        self.zip_state = StreamParser._State.DATA_DESCRIPTOR
+                    else:
+                        self.header = None
+                        self.zip_state = StreamParser._State.LOCAL_HEADER
                 else:
-                    # Get more data
                     break
-            # Data Descriptor
-            elif self.zip_state == StreamParser.State.DATA_DESCRIPTOR:
-                # Ensure there is enough data in the buffer
-                if self.buffer.getbuffer().nbytes < struct.calcsize(DataDescriptor.STATIC_FORMAT):
-                    break
-                # Read DataDescriptor
-                self.buffer.seek(0)
-                data = DataDescriptor.from_stream(self.buffer)
-                self.data_descriptor(data)
-                self.buffer = io.BytesIO(self.buffer.read())
-                # Transition to UNKNOWN state
-                self.zip_state = StreamParser.State.UNKNOWN
-            # Central Directory File Header
-            elif self.zip_state == StreamParser.State.CD_HEADER:
-                # Ensure there is enough data in the buffer
-                if self.buffer.getbuffer().nbytes < struct.calcsize(CDHeader.STATIC_FORMAT):
-                    break
-                self.buffer.seek(0)
+            elif self.zip_state == StreamParser._State.DATA_DESCRIPTOR:
+                # Attempt to parse data descriptor
                 try:
-                    data = CDHeader.from_stream(self.buffer)
-                except IncompleteParse:
+                    descriptor = _data_descriptor.DataDescriptor.from_memoryview(buf)
+                except exceptions.IncompleteParse:
                     break
-                self.cd_header(data)
-                self.buffer = io.BytesIO(self.buffer.read())
-                # Transition to UNKNOWN state
-                self.zip_state = StreamParser.State.UNKNOWN
-            # End Of Central Directory Record
-            elif self.zip_state == StreamParser.State.EOCD:
-                # Ensure there is enough data in the buffer
-                if self.buffer.getbuffer().nbytes < struct.calcsize(EOCD.STATIC_FORMAT):
-                    break
-                self.buffer.seek(0)
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(descriptor) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
+                self.header = None
+                self.zip_state = StreamParser._State.LOCAL_HEADER
+            elif self.zip_state == StreamParser._State.CENTRAL_DIRECTORY_HEADER:
+                # Attempt to parse central directory header
                 try:
-                    data = EOCD.from_stream(self.buffer)
-                except IncompleteParse:
+                    header = _central_directory_header.CentralDirectoryHeader.from_memoryview(
+                        buf
+                    )
+                except exceptions.IncompleteParse:
                     break
-                self.eocd(data)
-                self.buffer = io.BytesIO(self.buffer.read())
-                # Transition to UNKNOWN state
-                self.zip_state = StreamParser.State.UNKNOWN
+                except exceptions.BadMagic:
+                    logger.info("HIT")
+                    if buf[:4] == _zip64_eocd.MAGIC:
+                        self.zip_state = StreamParser._State.ZIP64_EOCD
+                        continue
+                    elif buf[:4] == _eocd.MAGIC:
+                        self.zip_state = StreamParser._State.EOCD
+                        continue
+                    else:
+                        raise exceptions.BadMagic(buf[:4].tobytes())
+                self.end_of_files = True
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(header) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
+            elif self.zip_state == StreamParser._State.ZIP64_EOCD:
+                # Attempt to parse Zip64 end of central directory
+                try:
+                    eocd = _zip64_eocd.Zip64EOCD.from_memoryview(buf)
+                except exceptions.IncompleteParse:
+                    break
+                logger.info("ZIP64 %s", eocd)
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(eocd) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
+                self.zip_state = StreamParser._State.ZIP64_EOCD_LOCATOR
+            elif self.zip_state == StreamParser._State.ZIP64_EOCD_LOCATOR:
+                # Attempt to parse Zip64 end of central directory locator
+                try:
+                    locator = _zip64_eocd_locator.Zip64EOCDLocator.from_memoryview(buf)
+                except exceptions.IncompleteParse:
+                    break
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(locator) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
+                self.zip_state = StreamParser._State.EOCD
+            elif self.zip_state == StreamParser._State.EOCD:
+                # Attempt to parse end of central directory
+                try:
+                    eocd = _eocd.EOCD.from_memoryview(buf)
+                except exceptions.IncompleteParse:
+                    break
+
+                # Prepare for next state
+                self.buffer = io.BytesIO(buf[len(eocd) :])
+                self.buffer.seek(self.buffer.getbuffer().nbytes)
             else:
-                logging.critical('Bad state')
-                raise Exception()
+                raise Exception("Bad state")
+
+    def flush(self):
+        raise NotImplementedError()
--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='pointevector-zip',
-    version='0.0.1',
+    version='0.0.2',
     author='Andrew Hoekstra',
     author_email='andrew@pointevector.com',
     url='https://github.com/Pointe-Vector/zip',
     packages=find_namespace_packages(),
-    install_requires=[
-        'dataclasses',
-    ],
+    install_requires=[],
 )
